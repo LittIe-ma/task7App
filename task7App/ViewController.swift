c18e3f298bb0a47217133fb49377467768199aee
yasu1125 <v22.m1125@gmail.com>
2021-04-07 02:07:44 +0900

    Initial Commit

---
+++ task7App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task7App
+//
+//  Created by yasudamasato on 2021/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
