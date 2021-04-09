ac5b2710b3faa6936b39b6c46ae9ee0fa32057fd
akio0911 <akio0911@gmail.com>
2021-04-09 09:50:09 +0900

    feedback

--- task7App/GreenViewController.swift
+++ task7App/GreenViewController.swift
@@ -18,31 +18,18 @@ class GreenViewController: UIViewController {
 
         textField1.keyboardType = .numberPad
         textField2.keyboardType = .numberPad
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func pressButton(_ sender: Any) {
 
         let number1 = Int(textField1.text!) ?? 0
         let number2 = Int(textField2.text!) ?? 0
-        let result: Int
 
-        result = number1 - number2
+        let result = number1 - number2
 
         resultLabel.text = "\(result)"
 
-        self.textField1.resignFirstResponder()
-        self.textField2.resignFirstResponder()
+        textField1.resignFirstResponder()
+        textField2.resignFirstResponder()
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
